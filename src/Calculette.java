7d0ba523ee8ea58fe6ed57632f2692e7de3f02a2
lcantinat <loric.cantinat@etudiant.univ-rennes1.fr>
2025-11-14 13:48:14 +0100

    initial commit

---
+++ src/Calculette.java
@@ -0,0 +1,14 @@
+public class Calculette {
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static int soustraction(int a, int b) {
+        return a - b ;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1 + 3 = " + addition(1,3));
+    }
+}
